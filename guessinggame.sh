10cbfd8335c489f39f3e98289b9c78fd46fbe1f7
jackgelinas <jackgeli@hotmail.com>
2023-09-25 14:12:14 -0400

    Made loop to query unit valid answer passed

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
+function queryuser {
+	echo "How many files are in the current directory? (Please enter answer as an integer)"
+	read answer
+}
 
+clear && queryuser
+
+while [[ ! $answer =~ ^[0-9]+$ ]]
+do
+	clear && echo "Your answer was not an integer, please try again." && queryuser
+done
