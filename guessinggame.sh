2b261e1cc94d593e50faa1a77ee839fdc7c1eb0b
jackgelinas <jackgeli@hotmail.com>
2023-09-25 16:01:30 -0400

    Working version of program

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,14 +1,39 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
+# Function to query user until valid input: queryuser
+
 function queryuser {
 	echo "How many files are in the current directory? (Please enter answer as an integer)"
 	read answer
+	while [[ ! $answer =~ ^[0-9]+$ ]]
+	do
+        	clear && echo "Your answer was not an integer, please try again." && queryuser
+	done
 }
 
-clear && queryuser
+# Assign number of files as variable: numfiles
 
-while [[ ! $answer =~ ^[0-9]+$ ]]
-do
-	clear && echo "Your answer was not an integer, please try again." && queryuser
-done
+numfiles=$(ls -al | wc -l) && numfiles=$(echo "$numfiles - 1" | bc)
+
+# Clear screen and ask user for answer, loop unit integer is input.
+
+queryuser
+
+# If statements for greater, less than, correct, or if non-integer to restart program
+
+if [[ $answer -gt $numfiles ]]
+then
+	clear && echo "Your answer was too high. Please try again."
+	bash guessinggame.sh
+elif [[ $answer -lt $numfiles ]]
+then
+	clear && echo "Your answer was too low. Please try again."
+	bash guessinggame.sh
+elif [[ $answer -eq $numfiles ]]
+then
+	clear && echo "Your answer was correct! Thanks for playing"
+else
+	clear && echo "Error. Please try again."
+	bash guessinggame.sh
+fi
